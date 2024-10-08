8a2a31788681ad6a6d40decc7749c39930093b3f
deijaparker <deijaparker1@outlook.com>
2024-10-08 16:35:44 -0700

    Add core React files and styling in src

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import BookSchedule from './components/Booklist';
+
+
+function App() {
+  return (
+    <div className="App">
+    <h1>📚 Deija's Upcoming Reads 📚</h1>
+     <BookSchedule />
+    </div>
+  );
+}
+
+export default App;
